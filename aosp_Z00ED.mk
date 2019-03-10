5ebb336e16678c8ccf2fa6ce362a5c7e8a515905
Roker2 <dimaminko2000@gmail.com>
2019-03-10 13:39:42 +0000

    Hello, AEX

--- aosp_Z00ED.mk
+++ aosp_Z00ED.mk
@@ -2,11 +2,11 @@
 $(call inherit-product, device/asus/Z00ED/full_Z00ED.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 #DEVICE_PACKAGE_OVERLAYS += device/asus/msm8916-common/overlay/lineage
 
-PRODUCT_NAME := lineage_Z00ED
+PRODUCT_NAME := aosp_Z00ED
 PRODUCT_DEVICE := Z00ED
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone 2 Laser
