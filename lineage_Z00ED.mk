bcbd420998a193bb5a781546636baf8b115c8409
Roker2 <dimaminko2000@gmail.com>
2020-01-26 13:19:48 +0300

    Z00ED: Add fingerprint build from WW

--- lineage_Z00ED.mk
+++ lineage_Z00ED.mk
@@ -34,8 +34,8 @@ PRODUCT_MANUFACTURER := asus
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="JP_Phone-user 6.0.1 MMB29P 13.1011.1711.16-20180103 release-keys" \
+    PRIVATE_BUILD_DESC="WW_Phone-user 6.0.1 MMB29P 13.1010.1612.53-20170202 release-keys" \
     TARGET_DEVICE="ZE500KL"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "asus/JP_Phone/ASUS_Z00E_2:6.0.1/MMB29P/13.1011.1711.16-20180103:user/release-keys"
+BUILD_FINGERPRINT := "asus/WW_Phone/ASUS_Z00E_2:6.0.1/MMB29P/13.1010.1612.53-20170202:user/release-keys"
