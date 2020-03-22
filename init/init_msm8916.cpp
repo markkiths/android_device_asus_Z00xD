d96e952974be770d43063d612bdc9c898012170a
Xsanori <elxsanori@gmail.com>
2020-03-22 22:45:38 +0300

    Z00xD: init: regenerate init for combined Z00xD

--- init/init_msm8916.cpp
+++ init/init_msm8916.cpp
@@ -1,99 +1,9 @@
-/*
-   Copyright (c) 2013, The Linux Foundation. All rights reserved.
-   Copyright (C) 2016 The CyanogenMod Project.
-
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions are
-   met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * Neither the name of The Linux Foundation nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-
-   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
-   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
-   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/sysinfo.h>
 #include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
+
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
-#include <android-base/properties.h>
 #include "vendor_init.h"
-#include "property_service.h"
-#include "log.h"
-
-using android::base::GetProperty;
-using android::init::property_set;
-
-static int read_file2(const char *fname, char *data, int max_size)
-{
-    int fd, rc;
-
-    if (max_size < 1)
-        return 0;
-
-    fd = open(fname, O_RDONLY);
-    if (fd < 0) {
-        ("failed to open '%s'\n", fname);
-        return 0;
-    }
-
-    rc = read(fd, data, max_size - 1);
-    if ((rc > 0) && (rc < max_size))
-        data[rc] = '\0';
-    else
-        data[0] = '\0';
-    close(fd);
-
-    return 1;
-}
-
-static void init_alarm_boot_properties()
-{
-    int boot_reason;
-    FILE *fp;
-
-    fp = fopen("/proc/sys/kernel/boot_reason", "r");
-    fscanf(fp, "%d", &boot_reason);
-    fclose(fp);
-
-    /*
-     * Setup ro.alarm_boot value to true when it is RTC triggered boot up
-     * For existing PMIC chips, the following mapping applies
-     * for the value of boot_reason:
-     *
-     * 0 -> unknown
-     * 1 -> hard reset
-     * 2 -> sudden momentary power loss (SMPL)
-     * 3 -> real time clock (RTC)
-     * 4 -> DC charger inserted
-     * 5 -> USB charger inserted
-     * 6 -> PON1 pin toggled (for secondary PMICs)
-     * 7 -> CBLPWR_N pin toggled (for external power supply)
-     * 8 -> KPDPWR_N pin toggled (power key pressed)
-     */
-    property_set("ro.alarm_boot", boot_reason == 3 ? "true" : "false");
-}
 
 void property_override(char const prop[], char const value[])
 {
@@ -106,15 +16,59 @@ void property_override(char const prop[], char const value[])
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
-void vendor_load_properties()
-{
-    property_override("ro.product.device", "ASUS_Z00E_2");	
-    property_override("ro.product.model", "ASUS_Z00ED");	
-    property_override("ro.build.product", "ZE500KL");
-}
-
 void property_override_dual(char const system_prop[], char const vendor_prop[], char const value[])
 {
     property_override(system_prop, value);
     property_override(vendor_prop, value);
+}
+
+bool property_equal_char(char const prop[], char const value[])
+{
+    bool ret = false;
+    char system_prop[PROP_VALUE_MAX];
+
+    __system_property_get(prop, system_prop);
+    if(strncmp(system_prop, value, 1) == 0) {
+        ret = true;
+    }
+    
+    return ret;
+}
+
+int device_model_get()
+{
+    int ret;
+
+    ret = 0;
+
+    if(property_equal_char("ro.boot.id.prj", "7") && property_equal_char("ro.boot.id.rf", "5")) {
+        // Z00ED
+        ret = 1;
+    } else if(property_equal_char("ro.boot.id.prj", "3") && property_equal_char("ro.boot.id.rf", "0"))  {
+        // Z00RD
+        ret = 2;
+    }
+
+    return ret;
+}
+
+void vendor_load_properties()
+{
+    int device_model;
+
+    device_model = device_model_get();
+
+    if (device_model == 1) {
+        property_override_dual("ro.product.device", "ro.product.vendor.device", "ASUS_Z00E_2");	
+        property_override_dual("ro.product.model", "ro.product.vendor.model", "ASUS_Z00ED");	
+        property_override("ro.build.product", "ZE500KL");
+        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "asus/WW_Phone/ASUS_Z00E_2:6.0.1/MMB29P/13.1010.1612.53-20170202:user/release-keys");
+        property_override("ro.build.description", "WW_Phone-user 6.0.1 MMB29P 13.1010.1612.53-20170202 release-keys");
+    } else if (device_model == 2) {
+        property_override_dual("ro.product.device", "ro.product.vendor.device", "ASUS_Z00RD_1");	
+        property_override_dual("ro.product.model", "ro.product.vendor.model", "ASUS_Z00RD");	
+        property_override("ro.build.product", "ZE500KG");
+        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "asus/WW_Phone/ASUS_Z00RD_1:6.0.1/MMB29P/13.10.10.25-20160523:user/release-keys");
+        property_override("ro.build.description", "WW_Phone-user 6.0.1 MMB29P 13.10.10.25-20160523 release-keys");
+    }
 }
