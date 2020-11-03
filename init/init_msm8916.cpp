5dafb895b4d73468fbac2cb386b8bc55841bcdd7
markkiths <markkiths@gmail.com>
2020-11-03 18:06:54 +0700

    Z00xD: Cleanup deprecated props
     * Let use override fingerprint & description in lineage_Z00xD.mk

--- init/init_msm8916.cpp
+++ init/init_msm8916.cpp
@@ -62,14 +62,10 @@ void vendor_load_properties()
         property_override_dual("ro.product.device", "ro.product.vendor.device", "ASUS_Z00E_2");	
         property_override_dual("ro.product.model", "ro.product.vendor.model", "ASUS_Z00ED");	
         property_override("ro.product.name", "ZE500KL");
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "asus/WW_Phone/ASUS_Z00E_2:6.0.1/MMB29P/13.1010.1612.53-20170202:user/release-keys");
-        property_override("ro.build.description", "WW_Phone-user 6.0.1 MMB29P 13.1010.1612.53-20170202 release-keys");
     } else if (device_model == 2) {
         property_override_dual("ro.product.device", "ro.product.vendor.device", "ASUS_Z00RD_1");	
         property_override_dual("ro.product.model", "ro.product.vendor.model", "ASUS_Z00RD");	
         property_override("ro.product.name", "ZE500KG");
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "asus/WW_Phone/ASUS_Z00RD_1:6.0.1/MMB29P/13.10.10.25-20160523:user/release-keys");
-        property_override("ro.build.description", "WW_Phone-user 6.0.1 MMB29P 13.10.10.25-20160523 release-keys");
         property_override("ro.power_profile.override", "power_profile_2");
     }
     // Init a dummy BT MAC address, will be overwritten later
